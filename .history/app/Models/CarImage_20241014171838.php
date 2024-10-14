374c93adb0c3a1522816e61c2233d25bb1dd40c3
Salah <sasa.wael2016@gmail.com>
2024-10-14 19:54:30 +0300

    handle Car Model and relation

---
+++ .history/app/Models/CarImage_20241014171838.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CarImage extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'car_id',
+        'image',
+    ];
+
+    protected $table = ''
+
+    public function carImage(){
+        return $this->belongsTo(Car::class, 'car_id', 'id');
+    }
+}
